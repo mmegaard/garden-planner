448427f49878feea9aa74f5dd79c08272d6cca31
Matt Megaard <mmegaard@gmail.com>
2026-04-27 18:49:59 -0700

    remove orphan motion/react import from GardenArea

--- src/components/GardenArea/GardenArea.tsx
+++ src/components/GardenArea/GardenArea.tsx
@@ -1,6 +1,5 @@
 "use client";
 import * as React from "react";
-import { motion, useMotionValue } from "motion/react";
 import GardenContainer from "../GardenContainer";
 import { Button } from "@radix-ui/themes";
 import { Move } from "react-feather";
